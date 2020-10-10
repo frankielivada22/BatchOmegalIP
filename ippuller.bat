5ff6c3ab58d3e72c8109a51f88edb13d11eadf6f
rustywolf021 <46093036+rustywolf021@users.noreply.github.com>
2020-10-10 14:00:54 +1300

    Update ippuller.bat

--- ippuller.bat
+++ ippuller.bat
@@ -4,19 +4,25 @@ cls
 
 color a
 echo:
-set /p tshark="Enter tshark derectory (C:\Program Files\Wireshark): "
+set /p tshark="Enter tshark derectory (Eg. -->> E:\Program Files\Wireshark): "
+echo Directory set to %tshark%
 echo:
 set /p interface="Whats your network interface (Ethernet (E), wi-fi (W), etc): "
-if %interface% == "E" goto :seth
-if %interface% == "W" goto :swif
+if "%interface%" == "E" goto :seth
+if "%interface%" == "W" goto :swif
 echo:
+
 :start
 echo Starting...
 echo:
 cd %tshark%
 echo:
-start tshark.exe -i %interface% -f udp -Y stun.att.ipv4-xord
+start tshark.exe -i %interface% -f udp -Y stun.att.ipv4-xord &
 timeout /t 2 /NOBREAK >nul
+
+:top
+echo Logging peoples ips, [you can close me if you want :) ]
+goto :top
 
 :seth
 set interface = "Ethernet"
@@ -25,3 +31,5 @@ goto :start
 :swif
 set interface = "wi-fi"
 goto :start
+
+
